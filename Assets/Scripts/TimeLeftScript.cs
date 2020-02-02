7eb26bd8f0ac2114599bdb6fe341a265c9ce7338
Faiz-gd <59527369+Faiz-gd@users.noreply.github.com>
2020-02-02 10:00:17 +0600

    Demyan lox

---
+++ Assets/Scripts/TimeLeftScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeLeftScript : MonoBehaviour
+{
+    Text text;
+    public static float timeleft = f;
+
+    void Start()
+    {
+        text = GetComponent<Text> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeleft -= Time.deltaTime;
+        if (timeleft < 0)
+            timeleft = 0;
+        text.text = "Тоби пизда: " + Mathf.Round (timeleft);
+    }
+}
