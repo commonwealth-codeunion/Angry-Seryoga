e11ef2817187b113c7b8f549237a56a9cc0f3ab4
Faiz-gd <59527369+Faiz-gd@users.noreply.github.com>
2020-02-02 07:50:24 +0600

    ccc

---
+++ Assets/Scripts/playercontroller.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+    public Player Player;
+    
+    private void Start()
+    {
+        Player = Player == null ? GetComponent<Player>() : Player;
+        if(Player ==null)
+        {
+            Debug.LogError("nothing");
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Player != null)
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                Player.MoveRight();
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                Player.MoveLeft();
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                Player.MoveUp();
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                Player.MoveDown();
+            }
+        }
+    }
+}
